21065c855c29e0fda0535f9ef4130a08786e7165
Lucas <lucasoliveiradiello@hotmail.com>
2024-07-14 18:57:25 -0300

    fix css

--- src/components/BackToTop.jsx
+++ src/components/BackToTop.jsx
@@ -4,7 +4,6 @@ import { IoIosArrowUp } from 'react-icons/io'
 const BackToTop = () => {
   return (
     <a href="#top" class="back-top-btn" aria-label="back to top" >
-    
     <IoIosArrowUp name="chevron-up" aria-hidden="true"></IoIosArrowUp>
   </a>
   )
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,7 +3,7 @@ import "../styles/hero.css"
 import { BiArrowFromLeft } from 'react-icons/bi'
 const Hero = () => {
   return (
-    <section className="section hero has-before has-bg-image hero-img h-[95vh]" id="home" aria-label="home">
+    <section className="section hero has-before has-bg-image hero-img" id="home" aria-label="home">
     <div className="containe p-block">
       <h1 className="h1 hero-title">
         Barba & Corte de Cabelo
